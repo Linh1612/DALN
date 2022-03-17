104d5972b932a5822a6eb5fdd4f45ccd329511d6
Phan Viet Linh <Linh.pv19010017@st.phenikaa-uni.edu.vn>
2022-03-17 17:24:11 +0700

    Laplich

---
+++ Laplich/Library.h
@@ -0,0 +1,19 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<vector>
+
+
+using namespace std;
+
+vector<string> tokenize(string const &str, char const delim) {
+    vector<string> out;
+    size_t start;
+    size_t end = 0;
+
+    while ((start = str.find_first_not_of(delim, end)) != string::npos) {
+        end = str.find(delim, start);
+        out.push_back(str.substr(start, end - start));
+    }
+    return out;
+}
